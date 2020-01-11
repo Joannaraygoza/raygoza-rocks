96b45641053663d8b4493b7e30404838e6aadcdf
Joanna Raygoza <jojojohnsto@gmail.com>
2020-01-11 17:43:18 -0600

    ThisIsMe initial save

---
+++ ThisIsMe.py
@@ -0,0 +1,12 @@
+def love(who):
+    print("Joanna loves", who)
+    print("Who do you love?")
+    #if who == "family":
+    #    print("Joanna loves her family")
+    #elif who == "husband":
+    #    print("Joanna loves Juan")
+    #elif who == "church life":
+    #    print("Joanna loves the church life")
+#     else:
+#         print("I'm only in love with three entities so far. What else shall I fall in love with?")
+love("Juan, Christ, and the church")
